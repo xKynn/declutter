1832b9473fd62ccc39352a30c91d0ee5273103ab
Ary Y <gtownx36@gmail.com>
2018-07-25 19:20:30 +0530

    Fix setup

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(
     version = '0.1',
     packages = ['declutter'],
     author = "xKynn",
+    author_email = "xKynn@github.com",
     description = "A commandline utility to declutter the files in current directory by organizing into folders automatically.",
     long_description=desc,
     long_description_content_type="text/markdown",
+    url="https://github.com/xKynn/declutter",
     classifiers=(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
